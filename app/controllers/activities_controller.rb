f9b51a7da1efb71e38ad050009831772347503bc
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-03-13 11:56:21 +1100

    create and update finalised

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,9 +1,70 @@
 class ActivitiesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+  before_action :set_form_vars, only: [:new, :edit]
+
   def index
     @activities= Activity.all
+   #@categories = Category.all
   end
 
   def show
-    @activity = Activity.find(params[:id])
+    
   end
+
+  def new
+    @activity = Activity.new
+    #@categories = Category.all
+  end
+
+  def create
+    @activity = current_user.activities.new(activity_params)
+    if @activity.save 
+      redirect_to @activity, notice: "Activity successfully created"
+    else
+      set_form_vars
+      render "new", notice: "Something went wrong"
+    end 
+  end
+
+  def edit
+
+  end
+
+  def update
+
+    @activity.update(activity_params)
+    if @activity.save 
+      redirect_to @activity, notice: "Activity successfully updated"
+    else
+      set_form_vars
+      render "edit", notice: "Something went wrong"
+    end 
+
+  end
+  
+  def destroy
+
+    @activity.destroy
+
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :price, :category_id, :description)
+  end
+
+  def set_activity
+
+    @activity = Activity.find(params[:id])
+
+  end
+
+  def set_form_vars
+    @categories = Category.all
+   
+  end 
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
-  get "/restricted", to: "pages#restricted" 
-  get "/activities", to: "activities#index", as: "activities"
-  get "/activities/:id", to: "activities#show", as: "activity"
+  get "restricted", to: "pages#restricted" 
+  get "activities", to: "activities#index", as: "activities"
+  post "activities", to: "activities#create"
+  get "activities/new", to: "activities#new", as: "new_activity"
+  get "activities/:id", to: "activities#show", as: "activity"
+  put "activities/:id", to: "activities#update"
+  patch "activities/:id", to: "activities#update"
+  delete "activities/:id", to: "activities#destroy"
+  get "activities/:id/edit", to: "activities#edit", as: "edit_activity"
 end
