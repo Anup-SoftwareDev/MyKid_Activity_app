3e4c55175e0a2afd89a9157a527d74ecf45456c4
Anup-SoftwareDev <joseph.anupsa@gmail.com>
2022-03-20 01:36:55 +1100

    Validation for Activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,9 @@ class Activity < ApplicationRecord
   belongs_to :user
   has_one_attached :picture
   has_many :registrations
+
+  #validations
+  validates :title, :description, :price, :category, presence: true
+  validates :title, length: {minimum: 4}
+  #validates :price, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
 end
